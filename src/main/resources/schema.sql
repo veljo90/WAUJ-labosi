bdb5f60b8d49f84fda49f452291986e76ef470f6
Velimir Lisec <lisec.velimir@gmail.com>
2018-04-14 18:20:57 +0200

    Add more tables to schema.sql
    Add Predavac, Predavanje and Predavanje_Predavac tables.
    
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,22 @@ create table if not exists KorisnikPrava (
 	username varchar(20) not null,
 	authority varchar(20) not null
 );
+
+create table if not exists Predavanje (
+	id identity,
+	tema varchar(50) not null,
+	sadrzaj varchar(300) not null,
+	vrsta varchar(20) not null,
+	objavljeno bit not null
+);
+
+create table if not exists Predavac(
+	id identity,
+	ime varchar(20) not null,
+	pozicija varchar(10) not null
+);
+
+create table if not exists Predavanje_Predavac (
+	predavanje bigint not null,
+	predavac bigint not null
+);
