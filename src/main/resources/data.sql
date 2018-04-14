a623b1da75375a7880bd733f1513e6f498b365ad
Velimir Lisec <lisec.velimir@gmail.com>
2018-04-14 15:49:14 +0200

    Create data.sql and add 2 users to database, admin and student
    Passwords are hashed using bcrypt level 4
    
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into Korisnik (username, password, enabled) 
+	values ('admin', '$2a$04$iUK/HUedCxtR4umVSnFADOyXwxizc7.kAe5hyzPHs7.xgHEBu4lF6', 1);
+
+insert into Korisnik (username, password, enabled)
+	values ('student', '$2a$04$M7WfkaktG11A/BK5KuZu3ubE9JfIHcagApUO/KSB/jx3Nf.LkYoCi', 1);
+
+insert into KorisnikPrava (username, authority) values ('admin', 'ROLE_ADMIN');
+insert into KorisnikPrava (username, authority) values ('admin', 'ROLE_USER');
+insert into KorisnikPrava (username, authority) values ('student', 'ROLE_USER');
