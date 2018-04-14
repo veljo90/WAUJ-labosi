cbff33a2e57c034205d88e35cd1e522a05dfff0b
Velimir Lisec <lisec.velimir@gmail.com>
2018-04-14 16:00:42 +0200

    Replace in-memory config with JDBC config
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/SecurityConfig.java
+++ src/main/java/hr/tvz/lisec/SecurityConfig.java
@@ -1,41 +1,45 @@
 package hr.tvz.lisec;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.inMemoryAuthentication()
-			.withUser("admin")
-			.password("{noop}adminpass")
-			.roles("ADMIN", "USER")
-			.and()
-			.withUser("student")
-			.password("{noop}studentpass")
-			.roles("USER");
+		 .jdbcAuthentication()
+		 	.dataSource(dataSource)
+		 	.usersByUsernameQuery("select username, password, enabled from Korisnik where username=?")
+		 	.authoritiesByUsernameQuery("select username, authority from KorisnikPrava where username=?")
+		 	.passwordEncoder(new BCryptPasswordEncoder(4));
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/predavanja/**")
-			.hasRole("USER")
-			.antMatchers("/**").permitAll()
+				.antMatchers("/predavanja/**")
+					.hasRole("USER")
+					.antMatchers("/**").permitAll()
 			.and()
-			.formLogin()
-			.loginPage("/login")
-			.defaultSuccessUrl("/predavanja/novo", true)
+				.formLogin()
+					.loginPage("/login")
+						.defaultSuccessUrl("/predavanja/novo", true)
 			.and()
-			.logout()
-			.logoutSuccessUrl("/login");
+				.logout()
+					.logoutSuccessUrl("/login");
 	}
 }
