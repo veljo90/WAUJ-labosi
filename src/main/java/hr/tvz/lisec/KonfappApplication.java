36ae335bb08930612710b697b9e0f08f08d6f03f
Velimir Lisec <lisec.velimir@gmail.com>
2018-04-22 20:45:32 +0200

    Disable Hibernate autoconfiguration
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/KonfappApplication.java
+++ src/main/java/hr/tvz/lisec/KonfappApplication.java
@@ -2,8 +2,9 @@ package hr.tvz.lisec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
 public class KonfappApplication {
 
 	public static void main(String[] args) {
