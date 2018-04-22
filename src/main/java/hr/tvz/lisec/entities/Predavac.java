7cfe35c8c27d34630aafc9c47055010694c7c4ae
Velimir Lisec <lisec.velimir@gmail.com>
2018-04-22 22:26:18 +0200

    Add @Entity anotation to Predavac.java
    Also set id as Identifier and autoincrement it
    
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/entities/Predavac.java
+++ src/main/java/hr/tvz/lisec/entities/Predavac.java
@@ -1,13 +1,22 @@
 package hr.tvz.lisec.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
 public class Predavac {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	
 	//@NotEmpty(message = "Niste unijeli ime predavača")
