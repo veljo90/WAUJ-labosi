7a40411c4571cb78b368aa4e08ffed037bfe3345
Velimir Lisec <lisec.velimir@gmail.com>
2018-04-25 21:00:48 +0200

    Add update and delete methods to PredavanjeRepository interface
    Also change findOne method to receive id of type long
    
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
+++ src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
@@ -4,6 +4,8 @@ import hr.tvz.lisec.entities.Predavanje;
 
 public interface PredavanjeRepository {
 	Iterable<Predavanje> findAll();
-	Predavanje findOne(String id);
+	Predavanje findOne(long id);
 	Predavanje save(Predavanje predavanje);
+	void update(Predavanje predavanje);
+	void delete(long id);
 }
