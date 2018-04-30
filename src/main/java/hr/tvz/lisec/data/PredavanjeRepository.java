999fc55e0286cb6f91bc2707bf95f25b0dc62356
Velimir Lisec <lisec.velimir@gmail.com>
2018-04-30 17:41:21 +0200

    Add update and delete methods to PredavanjeRepository.
    Aslo update findOne method to receive type Long instead of String
    
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
+++ src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
@@ -4,6 +4,8 @@ import hr.tvz.lisec.entities.Predavanje;
 
 public interface PredavanjeRepository {
 	Iterable<Predavanje> findAll();
-	Predavanje findOne(String id);
+	Predavanje findOne(Long id);
 	Predavanje save(Predavanje predavanje);
+	Predavanje update(Predavanje predavanje);
+	void delete(Long id);
 }
