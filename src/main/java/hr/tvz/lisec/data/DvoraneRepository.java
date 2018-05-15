6154beaac3f5b87e10a80da4e07757896d99b218
Velimir Lisec <lisec.velimir@gmail.com>
2018-05-15 17:44:56 +0200

    Add aditional methods to DvoraneRepository interface
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/data/DvoraneRepository.java
+++ src/main/java/hr/tvz/lisec/data/DvoraneRepository.java
@@ -4,5 +4,9 @@ import hr.tvz.lisec.entities.Dvorana;
 
 public interface DvoraneRepository {
 	Iterable<Dvorana> findAll();
+	Dvorana findOne(Long id);
 	Iterable<Dvorana> findAvailable();
+	Dvorana save(Dvorana dvorana);
+	Dvorana update(Dvorana dvorana);
+	void delete(Long id);
 }
