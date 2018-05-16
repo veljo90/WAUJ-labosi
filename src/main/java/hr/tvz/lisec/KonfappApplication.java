abb5f91e29d4cf6d515dfd5efbb169e2c3c6b75f
Velimir Lisec <lisec.velimir@gmail.com>
2018-05-16 16:24:37 +0200

    Remove Hibernate config
    and re-enable autoconfiguration
    
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/KonfappApplication.java
+++ src/main/java/hr/tvz/lisec/KonfappApplication.java
@@ -2,9 +2,8 @@ package hr.tvz.lisec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
+@SpringBootApplication
 public class KonfappApplication {
 
 	public static void main(String[] args) {
