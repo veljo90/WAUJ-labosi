85b8902b80d4bdd9cdc22d26af5d97df39f7e6c7
Velimir Lisec <lisec.velimir@gmail.com>
2018-05-16 21:02:12 +0200

    Add methods to search for all fields of predavanje to PredavanjeRepository
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
+++ src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
+import hr.tvz.lisec.entities.Predavac;
 import hr.tvz.lisec.entities.Predavanje;
 
 public interface PredavanjeRepository extends CrudRepository<Predavanje, Long> {
@@ -11,6 +12,9 @@ public interface PredavanjeRepository extends CrudRepository<Predavanje, Long> {
 	Optional<Predavanje> findById(Long id);
 	@SuppressWarnings("unchecked")
 	Predavanje save(Predavanje predavanje);
-	Iterable<Predavanje> findByTemaContaining(String tema);
+	Iterable<Predavanje> findByPredavac_ImeContainingAndPredavac_PozicijaAndTemaContainingIgnoreCaseAndSadrzajContainingAndVrstaAndObjavljeno(String ime, Predavac.Pozicija pozicija, String tema, String sadrzaj, Predavanje.Vrsta vrsta, Boolean objavljeno);
+	Iterable<Predavanje> findByPredavac_ImeContainingAndPredavac_PozicijaAndTemaContainingIgnoreCaseAndSadrzajContainingAndObjavljeno(String ime, Predavac.Pozicija pozicija, String tema, String sadrzaj, Boolean objavljeno);
+	Iterable<Predavanje> findByPredavac_ImeContainingAndTemaContainingIgnoreCaseAndSadrzajContainingAndVrstaAndObjavljeno(String ime, String tema, String sadrzaj, Predavanje.Vrsta vrsta, Boolean objavljeno);
+	Iterable<Predavanje> findByPredavac_ImeContainingAndTemaContainingIgnoreCaseAndSadrzajContainingAndObjavljeno(String ime, String tema, String sadrzaj, Boolean objavljeno);
 	void deleteById(Long id);
 }
