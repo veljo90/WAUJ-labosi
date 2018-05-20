d70d9769ebd154b3e93e66979c00816513e9511b
Velimir Lisec <lisec.velimir@gmail.com>
2018-05-20 20:24:02 +0200

    Replace REST controllers with Spring Data REST generated ones
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/data/PredavacRepository.java
+++ src/main/java/hr/tvz/lisec/data/PredavacRepository.java
@@ -3,9 +3,11 @@ package hr.tvz.lisec.data;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import hr.tvz.lisec.entities.Predavac;
 
+@RepositoryRestResource(path="/predavac")
 public interface PredavacRepository extends CrudRepository<Predavac, Long>{
 	Iterable<Predavac> findAll();
 	Optional<Predavac> findById(Long id);
--- src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
+++ src/main/java/hr/tvz/lisec/data/PredavanjeRepository.java
@@ -3,9 +3,11 @@ package hr.tvz.lisec.data;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import hr.tvz.lisec.entities.Predavanje;
 
+@RepositoryRestResource(path="/predavanje")
 public interface PredavanjeRepository extends CrudRepository<Predavanje, Long> {
 	Iterable<Predavanje> findAll();
 	Optional<Predavanje> findById(Long id);
