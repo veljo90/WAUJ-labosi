033ab932578bf1c663f935fb6bfa642074b93a12
Velimir Lisec <lisec.velimir@gmail.com>
2018-05-27 21:54:38 +0200

    Add creation timestamp to Predavanje class
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

--- src/main/java/hr/tvz/lisec/entities/Predavanje.java
+++ src/main/java/hr/tvz/lisec/entities/Predavanje.java
@@ -1,5 +1,7 @@
 package hr.tvz.lisec.entities;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,6 +25,8 @@ public class Predavanje {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	private LocalDateTime datumUpisa;
 	
 	@Valid
 	@OneToOne(targetEntity=Predavac.class, cascade=CascadeType.ALL)
