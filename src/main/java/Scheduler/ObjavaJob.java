ce52519518caae070218b916159e6807f9c3f76c
Velimir Lisec <lisec.velimir@gmail.com>
2018-05-27 22:02:00 +0200

    Add ObjavaJob class
    Signed-off-by: Velimir Lisec <lisec.velimir@gmail.com>

---
+++ src/main/java/Scheduler/ObjavaJob.java
@@ -0,0 +1,36 @@
+package Scheduler;
+
+import java.time.LocalDateTime;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import hr.tvz.lisec.data.PredavanjeRepository;
+import hr.tvz.lisec.entities.Predavanje;
+
+public class ObjavaJob extends QuartzJobBean {
+	
+	@Autowired
+	PredavanjeRepository predavanjeRepository;
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		Iterable<Predavanje> predavanja = predavanjeRepository.findAll();
+		
+		for(Predavanje predavanje : predavanja) {
+			if (novoObjavljenoPredavanje(predavanje)) {
+				objaviPredavanje(predavanje);
+			}
+		}
+	}
+	
+	private Boolean novoObjavljenoPredavanje(Predavanje predavanje) {
+		return predavanje.getObjavljeno() && predavanje.getDatumUpisa().isAfter(LocalDateTime.now().minusSeconds(10));
+	}
+	
+	private void objaviPredavanje(Predavanje predavanje) {
+		System.out.println("Objavljeno je novo predavanje: " + predavanje.getTema());
+	}
+}
